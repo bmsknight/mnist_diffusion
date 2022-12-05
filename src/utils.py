e9d8f5de9bb128868aa916dc94e1ef6152bcd6e7
bmsknight <bmsknight@gmail.com>
2022-12-05 15:09:23 -0500

    Add removing points in batch

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 
 def remove_random_points(datapoint, missing_frac):
@@ -14,3 +15,16 @@ def remove_random_points(datapoint, missing_frac):
     datapoint = np.expand_dims(datapoint, 0)
     mask = np.expand_dims(mask, 0)
     return datapoint, mask
+
+
+def remove_random_points_tensor(batch_data, missing_frac):
+    missing_batch = batch_data.squeeze()
+    mask = torch.ones_like(missing_batch)
+    length = missing_batch.shape[1]
+    missing_frac = round(length * missing_frac)
+    indices = torch.multinomial(mask, missing_frac)
+    missing_batch = missing_batch.scatter(dim=1, index=indices, value=0)
+    mask = mask.scatter(dim=1, index=indices, value=0)
+    missing_batch = missing_batch.unsqueeze(dim=1)
+    mask = mask.unsqueeze(dim=1)
+    return missing_batch, mask
